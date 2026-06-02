a318bd7caf3b06e27323f3bc78cd8250c9a14ea8
MsdAmine <moussaid.amine19@gmail.com>
2026-06-02 01:17:29 +0100

    feat: implement LessonProgress entity and repository infrastructure #57

---
+++ backend/src/main/java/com/learnova/learnova_backend/course/entity/Lesson.java
@@ -0,0 +1,21 @@
+package com.learnova.learnova_backend.course.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "lessons")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+}
